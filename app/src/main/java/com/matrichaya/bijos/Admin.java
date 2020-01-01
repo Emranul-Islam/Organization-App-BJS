19da81944d2fd5d16192d54f63c5a00bbc6d0518
Emranul-Islam <emranul.islam.sohag@gmail.com>
2020-01-02 02:32:46 +0600

    Bug fixes

--- app/src/main/java/com/matrichaya/bijos/Admin.java
+++ app/src/main/java/com/matrichaya/bijos/Admin.java
@@ -1,10 +1,11 @@
 package com.matrichaya.bijos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class Admin extends AppCompatActivity {
--- app/src/main/java/com/matrichaya/bijos/MainActivity.java
+++ app/src/main/java/com/matrichaya/bijos/MainActivity.java
@@ -2,11 +2,11 @@ package com.matrichaya.bijos;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
